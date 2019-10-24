f483fc4fe8c45c5025d1384e4a3e172ded646b9a
Tom Bricaud <tom.bricaud@itera.io>
2019-10-24 11:37:20 +0200

    min32 and max32

--- computations.go
+++ computations.go
@@ -3,3 +3,17 @@ package main
 func FromRGB(r, g, b uint8) uint32 {
 	return Red*uint32(r)/255 + Green*uint32(g)/255 + Blue*uint32(b)/255
 }
+
+func Max32(x, y int32) int32 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func Min32(x, y int32) int32 {
+	if x < y {
+		return x
+	}
+	return y
+}
