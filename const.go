313c9679c16faddacafb15bfc21fd7cf6705b278
Tom Bricaud <tom.bricaud@itera.io>
2019-10-24 11:37:32 +0200

    added more const

--- const.go
+++ const.go
@@ -12,15 +12,18 @@ const (
 	GameStepDuration = 10 * time.Millisecond
 	StepsPerFrame    = 10
 	PlayerStep       = 4
-	Gravity          = 4
-	JumpPower        = 80
+	Gravity          = 30
+	JumpPower        = 800
+	AirMovePower     = 20
+	AirSlow          = 4
+	InertiaPerPixel  = 100
 
 	WindowWidth, WindowHeight = 1120, 630
 
+	TileWidth, TileHeight = 25, 25
+
 	PlayerSpriteSheet                     = "resources/sprites/player/adventurer-v1.5-Sheet.png" // see playeranimations.go
 	PlayerSpriteWidth, PlayerSpriteHeight = 50, 37
-
-	TileWidth, TileHeight = 25, 25
 
 	FontFile, FontSize = "resources/fonts/DejaVuSans.ttf", 18
 
