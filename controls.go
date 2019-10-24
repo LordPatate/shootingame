a3004fe6551676efe2389e96bc0353273026cb00
Tom Bricaud <tom.bricaud@itera.io>
2019-10-24 11:38:21 +0200

    renamed Movements to GroundSteps

--- controls.go
+++ controls.go
@@ -4,7 +4,7 @@ import "github.com/veandco/go-sdl2/sdl"
 
 const left, right = true, false
 
-var Movements map[sdl.Scancode]func(*Player_t) = map[sdl.Scancode]func(*Player_t){
+var GroundSteps map[sdl.Scancode]func(*Player_t) = map[sdl.Scancode]func(*Player_t){
 	sdl.SCANCODE_A: func(player *Player_t) { player.Step(left) },
 	sdl.SCANCODE_D: func(player *Player_t) { player.Step(right) },
 }
