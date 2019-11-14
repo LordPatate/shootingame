22d908cfb4a3d0fc4a7ca82cbbfed48c856af5dc
Tom Bricaud <tom.bricaud@itera.io>
2019-11-14 15:46:53 +0100

    Main classes

---
+++ cs/Game.cs
@@ -0,0 +1,11 @@
+using System;
+using SDL2;
+
+namespace shootingame
+{
+    class Game
+    {
+        public static bool Running;
+        public static Player Player;
+    }
+}
---
+++ cs/Popup.cs
@@ -0,0 +1,10 @@
+using System;
+using SDL2;
+
+namespace shootingame
+{
+    class Popup
+    {
+        
+    }
+}
