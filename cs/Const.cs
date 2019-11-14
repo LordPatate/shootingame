7373c7c18d367185dd8fc5eeefe14c34282ed816
Tom Bricaud <tom.bricaud@itera.io>
2019-11-14 15:09:50 +0100

    const

---
+++ cs/Const.cs
@@ -0,0 +1,30 @@
+namespace shootingame
+{
+    class Const
+    {
+        public const uint Red = 0x00ff0000, Green = 0x0000ff00,  Blue =  0x000000ff;
+
+        public const bool Left = true, Right = false;
+
+        public const int GameStepDuration = 10,
+        StepsPerFrame = 10,
+        PlayerStep = 5,
+        Gravity = 30,
+        JumpPower = 850,
+        AirMovePower = 20,
+        AirSlow = 4,
+        WallFriction = 10,
+        InertiaPerPixel = 100,
+        HookMaxRange = 170;
+
+        public const int WindowWidth = 1120, WindowHeight = 630,
+        TileWidth = 25, TileHeight = 25;
+
+        public const string PlayerSpriteSheet = "../resources/sprites/player/adventurer-v1.5-Sheet.png"; // see playeranimations.go
+        public const int PlayerSpriteWidth = 50, PlayerSpriteHeight = 37,
+        PlayerScalePercent = 120;
+
+        public const string FontFile = "../resources/fonts/DejaVuSans.ttf";
+        public const int FontSize = 18;
+    }
+}
--- cs/Program.cs
+++ cs/Program.cs
@@ -9,11 +9,18 @@ namespace shootingame
         static void Main(string[] args)
         {
             SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
-            SDL.SDL_CreateWindow("Shootingame", SDL.SDL_WINDOWPOS_UNDEFINED, SDL.SDL_WINDOWPOS_UNDEFINED, 1120, 630, SDL.SDL_WindowFlags.SDL_WINDOW_FULLSCREEN_DESKTOP);
+            SDL_ttf.TTF_Init();
+            SDL_image.IMG_Init(SDL_image.IMG_InitFlags.IMG_INIT_PNG);
+            
+            SDL.SDL_CreateWindow("Shootingame", SDL.SDL_WINDOWPOS_UNDEFINED, SDL.SDL_WINDOWPOS_UNDEFINED,
+            Const.WindowWidth, Const.WindowHeight,
+            SDL.SDL_WindowFlags.SDL_WINDOW_FULLSCREEN_DESKTOP);
 
             Thread.Sleep(1000);            
 
             SDL.SDL_Quit();
+            SDL_ttf.TTF_Quit();
+            SDL_image.IMG_Quit();
         }
     }
 }
