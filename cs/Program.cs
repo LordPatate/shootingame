95e0bc0aae31a1f79cc3f08cf65bb745b84c1cd1
Tom Bricaud <tom.bricaud@itera.io>
2019-11-14 14:23:22 +0100

    SDL2-CS

--- cs/Program.cs
+++ cs/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using SDL2;
 
 namespace shootingame
 {
@@ -6,7 +8,12 @@ namespace shootingame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
+            SDL.SDL_CreateWindow("Shootingame", SDL.SDL_WINDOWPOS_UNDEFINED, SDL.SDL_WINDOWPOS_UNDEFINED, 1120, 630, SDL.SDL_WindowFlags.SDL_WINDOW_FULLSCREEN_DESKTOP);
+
+            Thread.Sleep(1000);            
+
+            SDL.SDL_Quit();
         }
     }
 }
