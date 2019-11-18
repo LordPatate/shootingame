3ead4d67564b2ab5695d1309d17661aad40c9059
Tom Bricaud <tom.bricaud@itera.io>
2019-11-18 10:19:27 +0100

    errors update

--- cs/Errors.cs
+++ cs/Errors.cs
@@ -5,15 +5,33 @@ namespace shootingame
 {
     class Errors
     {
+        public static string msg = "";
         public static void Check(int val, string msg = "")
         {
             if (val == 0)
                 return;
             
+            if (msg != "")
+                Errors.msg = msg;
+            Print();
+        }
+
+        public static void CheckNull(IntPtr ptr, string msg = "")
+        {
+            if (ptr != null)
+                return;
+
+            if (msg != "")
+                Errors.msg = msg;
+            Print();
+        }
+
+        private static void Print()
+        {
             if (msg == "")
                 msg = "Error";
             
-            throw new Exception("%s: %s", msg, SDL.SDL_GetError());
+            throw new Exception($"{msg}: {SDL.SDL_GetError()}");
         }
     }
 }
