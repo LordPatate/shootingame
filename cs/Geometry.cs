e707c239f8956b523a3c59fe2d9745103ae0769e
Tom Bricaud <tom.bricaud@itera.io>
2019-11-22 14:11:16 +0100

    geometry

--- cs/Geometry.cs
+++ cs/Geometry.cs
@@ -1,22 +1,18 @@
-using SDL2;
+using SFML.Graphics;
+using SFML.System;
 
 namespace shootingame
 {
     class Geometry
     {
-        public static bool PointInRectangle(int x, int y, SDL.SDL_Rect r) {
-            return r.x < x && r.x+r.w > x &&
-                r.y < y && r.y+r.h > y;
-        }
-
-        public static SDL.SDL_Rect ScaleRect(SDL.SDL_Rect rect, int wPercent, int hPercent)
+        public static IntRect ScaleRect(IntRect rect, int wPercent, int hPercent)
         {
-            int width = rect.w * wPercent / 100;
-            int height = rect.h * hPercent / 100;
-            return SDLFactory.MakeRect(
-                x: rect.x + (rect.w-width)/2,
-                y: rect.y + (rect.h-height)/2,
-                w: width, h: height
+            int width = rect.Width * wPercent / 100;
+            int height = rect.Height * hPercent / 100;
+            return new IntRect(
+                left: rect.Left + (rect.Width-width)/2,
+                top: rect.Top + (rect.Height-height)/2,
+                width: width, height: height
             );
         }
     }
